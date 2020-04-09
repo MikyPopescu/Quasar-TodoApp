3ff39d926ffd9770750047557fc44fd1cc9eb3b2
MikyPopescu <popescu9andreea17@stud.ase.ro>
2020-04-09 18:52:44 +0300

    routes and navigation

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,107 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>Quasar App</q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab to="/" exact clickable icon="list" label="Todo" />
+        <q-route-tab to="/settings" exact clickable icon="settings" label="Settings" />
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <q-list>
+        <q-item-label header class="text-grey-8">Navigation</q-item-label>
+
+        <q-item to="/" exact clickable>
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Todo</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item to="/settings" exact clickable>
+          <q-item-section avatar>
+            <q-icon name="settings" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Settings</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+
+  components: {},
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: "Docs",
+          caption: "quasar.dev",
+          icon: "school",
+          link: "https://quasar.dev"
+        },
+        {
+          title: "Github",
+          caption: "github.com/quasarframework",
+          icon: "code",
+          link: "https://github.com/quasarframework"
+        },
+        {
+          title: "Discord Chat Channel",
+          caption: "chat.quasar.dev",
+          icon: "chat",
+          link: "https://chat.quasar.dev"
+        },
+        {
+          title: "Forum",
+          caption: "forum.quasar.dev",
+          icon: "record_voice_over",
+          link: "https://forum.quasar.dev"
+        },
+        {
+          title: "Twitter",
+          caption: "@quasarframework",
+          icon: "rss_feed",
+          link: "https://twitter.quasar.dev"
+        },
+        {
+          title: "Facebook",
+          caption: "@QuasarFramework",
+          icon: "public",
+          link: "https://facebook.quasar.dev"
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,20 +1,21 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/Layout.vue"),
+    //paths to my pages
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: "", component: () => import("pages/PageTodo.vue") },
+      { path: "/settings", component: () => import("pages/PageSettings.vue") }
     ]
   }
-]
+];
 
 // Always leave this as last one
-if (process.env.MODE !== 'ssr') {
+if (process.env.MODE !== "ssr") {
   routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
 }
 
-export default routes
+export default routes;
