e4cec9a220f4915bead8befc9412218fd9ba96df
MikyPopescu <popescu9andreea17@stud.ase.ro>
2020-04-12 20:38:34 +0300

    combined duplicated code

--- src/components/Tasks/Modals/EditTask.vue
+++ src/components/Tasks/Modals/EditTask.vue
@@ -19,7 +19,9 @@
 
 <script>
 import { mapActions } from "vuex";
+import mixinAddEditTask from "src/mixins/mixin-add-edit-task";
 export default {
+  mixins: [mixinAddEditTask],
   props: ["task", "id"],
   data() {
     return {
@@ -28,13 +30,7 @@ export default {
   },
   methods: {
     ...mapActions("tasks", ["updateTask"]),
-    submitForm() {
-      // console.log("submitForm");
-      this.$refs.modalTaskName.$refs.name.validate();
-      if (!this.$refs.modalTaskName.$refs.name.hasError) {
-        this.submitTask();
-      }
-    },
+
     submitTask() {
       //console.log("submitTask");
       this.updateTask({
@@ -42,23 +38,7 @@ export default {
         updates: this.taskToSubmit
       });
       this.$emit("close");
-    },
-    clearDueDate() {
-      this.taskToSubmit.dueDate = "";
-      this.taskToSubmit.dueTime = "";
     }
-  },
-  components: {
-    "modal-header": require("components/Tasks/Modals/Shared/ModalHeader.vue")
-      .default,
-    "modal-task-name": require("components/Tasks/Modals/Shared/ModalTaskName.vue")
-      .default,
-    "modal-due-date": require("components/Tasks/Modals/Shared/ModalDueDate.vue")
-      .default,
-    "modal-due-time": require("components/Tasks/Modals/Shared/ModalDueTime.vue")
-      .default,
-    "modal-buttons": require("components/Tasks/Modals/Shared/ModalButtons.vue")
-      .default
   },
   mounted() {
     this.taskToSubmit = Object.assign({}, this.task);
